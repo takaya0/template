9d82066d15100257380e1b04abde3d492593131f
takaya0 <33962896+takaya0@users.noreply.github.com>
2019-03-27 01:14:13 +0900

    Add files via upload

---
+++ cpp/base-kpro.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <math.h>
+#include <string>
+#include <string.h>
+#include <algorithm>
+#include <vector>
+#include <set>
+#include <map>
+#include <stack>
+#include <queue>
+#include <stdlib.h>
+#include <deque>  
+#include <iomanip>
+
+using namespace std;
+
+using ll = long long;
+#define INF le9
+#define MOD le9 + 7
+#define rep(i, n) for(int i = 0; i < n; i++)
+#define loop(i, a, n) for(int i = a; i < n; i++)
